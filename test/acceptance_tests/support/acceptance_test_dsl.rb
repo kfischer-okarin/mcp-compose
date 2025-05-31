42238dc828677414304fff213b3e8ea0e02eb486
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-31 12:16:41 +0900

    Skip when acceptnace test method is missing

--- test/acceptance_tests/support/acceptance_test_dsl.rb
+++ test/acceptance_tests/support/acceptance_test_dsl.rb
@@ -4,4 +4,18 @@ module AcceptanceTestDSL
   def do_something
     # Implementation
   end
+
+  def method_missing(name, *args, &block)
+    message = <<~MESSAGE
+      undefined DSL method `#{name}`
+
+      Available methods:
+    MESSAGE
+    AcceptanceTestDSL.instance_methods(false).sort.each do |method|
+      next if method == :method_missing
+
+      message << "- #{method}\n"
+    end
+    skip(message)
+  end
 end
