dfec99746993f3202a3565d6726abb6fe53a9cbe
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-31 14:14:02 +0900

    Prepare base dir for acceptance test

--- test/acceptance_tests/support/acceptance_test_dsl.rb
+++ test/acceptance_tests/support/acceptance_test_dsl.rb
@@ -4,7 +4,7 @@
 # Private methods that are not used to express the test cases should be
 # defined in AcceptanceTestUtils.
 module AcceptanceTestDSL
-  def do_something
-    # Implementation
+  def given_a_mcp_server(name, &block)
+    ensure_base_dir_is_prepared
   end
 end
--- test/acceptance_tests/support/acceptance_test_utils.rb
+++ test/acceptance_tests/support/acceptance_test_utils.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require "pathname"
+
 # This module contains private utility methods for acceptance tests which are
 # not listed among the available DSL methods.
 module AcceptanceTestUtils
   private
+
+  def ensure_base_dir_is_prepared
+    @base_dir ||= Pathname.new(Dir.mktmpdir("mcp-compose-acceptance-tests"))
+  end
 
   def method_missing(name, *args, &block)
     message = <<~MESSAGE
