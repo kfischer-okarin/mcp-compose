3f5904ac26a4c78fb23f5c4a185728a14dc61820
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-31 13:41:39 +0900

    Rewrite let statements so they can be used inside dsl blocks

--- test/acceptance_tests/forwarding_tools_test.rb
+++ test/acceptance_tests/forwarding_tools_test.rb
@@ -3,38 +3,12 @@
 require_relative "support/acceptance_test_helper"
 
 feature "Forwarding Tools" do
-  let(:weather_tool) {
-    {
-      name: "weather",
-      description: "Get the weather",
-      inputSchema: {
-        type: "object",
-        properties: {
-          location: {
-            type: "string",
-            description: "The location to get the weather for"
-          }
-        }
-      }
-    }
-  }
-  let(:hello_tool) {
-    {
-      name: "hello",
-      description: "Say hello",
-      inputSchema: {
-        type: "object",
-        properties: {}
-      }
-    }
-  }
-
   scenario "forwards the tools of a server" do
     given_a_mcp_server("weather-mcp") {
-      with_tools weather_tool
+      with_tools Tools.weather
     }
     given_a_mcp_server("hello-mcp") {
-      with_tools hello_tool
+      with_tools Tools.hello
     }
     given_a_mcp_compose_file <<~YAML
       name: My Tools
@@ -54,5 +28,36 @@ feature "Forwarding Tools" do
     value(list_tools_result.length).must_equal(2)
     value(list_tools_result).must_include(weather_tool)
     value(list_tools_result).must_include(hello_tool)
+  end
+
+  module Tools
+    module_function
+
+    def weather
+      {
+        name: "weather",
+        description: "Get the weather",
+        inputSchema: {
+          type: "object",
+          properties: {
+            location: {
+              type: "string",
+              description: "The location to get the weather for"
+            }
+          }
+        }
+      }
+    end
+
+    def hello
+      {
+        name: "hello",
+        description: "Say hello",
+        inputSchema: {
+          type: "object",
+          properties: {}
+        }
+      }
+    end
   end
 end
