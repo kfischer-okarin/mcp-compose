6674328228aadff43ad93360801d429db37d67aa
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-06-11 23:04:51 +0900

    Refactor tests

--- test/mcp_compose/cli_test.rb
+++ test/mcp_compose/cli_test.rb
@@ -5,24 +5,29 @@ require_relative "../test_helper"
 
 module MCPCompose
   describe CLI do
+    let(:built_server) { Mock.new }
     let(:parse_config) { Mock.new }
-    let(:build_server) { Mock.new }
+    let(:build_server) {
+      server = built_server
+      Mock.new do |mock|
+        mock.method(:call).returns(server)
+      end
+    }
     let(:shell_context) { Mock.new }
     let(:cli) { CLI.new(parse_config_function: parse_config, build_server_function: build_server) }
 
     it "parses the mcp-compose.yml file in the current directory and uses it to start the server" do
       shell_context.mock.method(:read_file).expects_call_with("mcp-compose.yml").returns(:mcp_compose_content)
       parse_config.mock.method(:call).expects_call_with(:mcp_compose_content).returns(:parse_result)
-      server = Mock.new
-      build_server.mock.method(:call).expects_call_with(:parse_result).returns(server)
-      server.mock.method(:run).expects_call
+      build_server.mock.method(:call).expects_call_with(:parse_result).returns(built_server)
+      built_server.mock.method(:run).expects_call
 
       cli.run(shell_context: shell_context, args: [])
 
       shell_context.mock.assert_expected_calls_received
       parse_config.mock.assert_expected_calls_received
       build_server.mock.assert_expected_calls_received
-      server.mock.assert_expected_calls_received
+      built_server.mock.assert_expected_calls_received
     end
 
     it "returns a nice error message if the mcp-compose.yml file is not found" do
